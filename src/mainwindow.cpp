040233bbf4912e360fb24c0aea83c7140fce980b
garak <garakmon@gmail.com>
2019-06-13 12:14:49 -0400

    extend wild encounter tab for all fields (dynamic)

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -259,11 +259,13 @@ bool MainWindow::openProject(QString dir) {
         setWindowTitle(editor->project->getProjectTitle());
         loadDataStructures();
         populateMapList();
+        populateWildMonTabWidget(ui->tabWidget_WildMons, editor->project->wildMonFields);
         success = setMap(getDefaultMap(), true);
     } else {
         setWindowTitle(editor->project->getProjectTitle());
         loadDataStructures();
         populateMapList();
+        populateWildMonTabWidget(ui->tabWidget_WildMons, editor->project->wildMonFields);
     }
 
     if (success) {
@@ -1082,7 +1084,7 @@ void MainWindow::on_tabWidget_currentChanged(int index)
     } else if (index == 3) {
         editor->setEditingConnections();
     } else if (index == 4) {
-        editor->setEditingWildMons();
+        //editor->setEditingWildMons();
     }
 }
 
